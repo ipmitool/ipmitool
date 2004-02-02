1070546b2ab104edc2055b60070d2a045403dfb6
Fredrik Öhrn <ohrn@chl.chalmers.se>
2004-02-02 17:45:57 +0000

    Add a README file. Add an alternative webpage script. Adjust the graphs a bit.

--- ipmitool/contrib/create_webpage_compact.sh
+++ ipmitool/contrib/create_webpage_compact.sh
@@ -71,7 +71,7 @@ for line in `eval $ipmi_cmd -c -v sdr list full` ; do
 	split=($line)
 
 	file="$rrd_dir/$hostname-${split[0]}.rrd"
-	group=`echo "${split[2]} ${split[*]:10:6}" | tr ' .-' ___`
+	group=`echo "${split[2]}" | tr ' .-' ___`
 
 	group_color=${group}_color
 
@@ -82,12 +82,6 @@ for line in `eval $ipmi_cmd -c -v sdr list full` ; do
 
 		group_unit=${group}_unit
 		declare $group_unit="${split[2]}"
-
-		group_title=${group}_title
-		declare $group_title="${split[5]} / ${split[6]}"
-
-		group_thres=${group}_thres
-		declare $group_thres="${split[10]},${split[11]},${split[12]},${split[13]},${split[14]},${split[15]}"
 	fi
 
 	declare $group="${!group}
@@ -104,64 +98,10 @@ IFS=" "
 for group in $groups ; do
 
 	group_unit=${group}_unit
-	group_title=${group}_title
-	group_thres=${group}_thres
 
 	IFS=,
 
-	split=(${!group_thres})
-
-	thres=
-
-	if [ -n "${split[0]}" ] ; then
-		if [ -n "${split[3]}" ] ; then
-			thres="
-  HRULE:${split[0]}#000000
-  HRULE:${split[3]}#000000:\"Upper & lower non-recoverable thresholds\""
-		else
-			thres="
-  HRULE:${split[0]}#000000:\"Upper non-recoverable threshold\""
-		fi
-	else
-		if [ -n "${split[3]}" ] ; then
-			thres="
-  HRULE:${split[3]}#000000:\"Lower non-recoverable threshold\""
-		fi
-	fi
-
-	if [ -n "${split[1]}" ] ; then
-		if [ -n "${split[4]}" ] ; then
-			thres="$thres
-  HRULE:${split[1]}#FF0000
-  HRULE:${split[4]}#FF0000:\"Upper & lower critical thresholds\""
-		else
-			thres="$thres
-  HRULE:${split[1]}#FF0000:\"Upper critical threshold\""
-		fi
-	else
-		if [ -n "${split[4]}" ] ; then
-			thres="$thres
-  HRULE:${split[4]}#FF0000:\"Lower critical threshold\""
-		fi
-	fi
-
-	if [ -n "${split[2]}" ] ; then
-		if [ -n "${split[5]}" ] ; then
-			thres="$thres
-  HRULE:${split[2]}#FFCC00
-  HRULE:${split[5]}#FFCC00:\"Upper & lower warning thresholds\""
-		else
-			thres="$thres
-  HRULE:${split[2]}#FFCC00:\"Upper warning threshold\""
-		fi
-	else
-		if [ -n "${split[5]}" ] ; then
-			thres="$thres
-  HRULE:${split[5]}#FFCC00:\"Lower warning threshold\""
-		fi
-	fi
-
-	echo "<h3>${!group_title}</h3>"
+	echo "<h3>${!group_unit}</h3>"
 
 	if [ "$graph_daily" -ne 0 ] ; then
 		cat << EOF
@@ -171,7 +111,7 @@ for group in $groups ; do
   --vertical-label "${!group_unit}"
   --title "Daily graph"
   --height $graph_height
-  --width $graph_width ${!group} $thres
+  --width $graph_width ${!group}
 >
 EOF
 	fi
@@ -185,7 +125,7 @@ EOF
   --vertical-label "${!group_unit}"
   --title "Weelky graph"
   --height $graph_height
-  --width $graph_width ${!group} $thres
+  --width $graph_width ${!group}
 >
 EOF
 	fi
@@ -199,7 +139,7 @@ EOF
   --vertical-label "${!group_unit}"
   --title "Monthly graph"
   --height $graph_height
-  --width $graph_width ${!group} $thres
+  --width $graph_width ${!group}
 >
 EOF
 	fi
