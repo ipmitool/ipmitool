2e1f1ef750d4dafef5a17879724bccd00ea6f8b3
Duncan Laurie <duncan@iceblink.org>
2004-11-17 23:25:16 +0000

    fix compile on freebsd

--- ipmitool/lib/ipmi_session.c
+++ ipmitool/lib/ipmi_session.c
@@ -37,11 +37,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
 
--- ipmitool/lib/ipmi_sol.c
+++ ipmitool/lib/ipmi_sol.c
@@ -37,9 +37,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/select.h>
 #include <sys/time.h>
-#include <sys/types.h>
 #include <signal.h>
 #include <unistd.h>
 
