d52c8f01c57a77faf61e2b760a3236e07a98fb43
Duncan Laurie <duncan@iceblink.org>
2004-11-17 22:19:15 +0000

    move strings to lib/ipmi_strings.h add entity id structure for lookups

--- ipmitool/include/ipmitool/ipmi_entity.h
+++ ipmitool/include/ipmitool/ipmi_entity.h
@@ -39,95 +39,18 @@
 
 #include <ipmitool/helper.h>
 
-const struct valstr entity_id_vals[] __attribute__((unused)) = {
-	{ 0x00, "Unspecified" },
-	{ 0x01, "Other" },
-	{ 0x02, "Unknown" },
-	{ 0x03, "Processor" },
-	{ 0x04, "Disk or Disk Bay" },
-	{ 0x05, "Peripheral Bay" },
-	{ 0x06, "System Management Module" },
-	{ 0x07, "System Board" },
-	{ 0x08, "Memory Module" },
-	{ 0x09, "Processor Module" },
-	{ 0x0a, "Power Supply" },
-	{ 0x0b, "Add-in Card" },
-	{ 0x0c, "Front Panel Board" },
-	{ 0x0d, "Back Panel Board" },
-	{ 0x0e, "Power System Board" },
-	{ 0x0f, "Drive Backplane" },
-	{ 0x10, "System Internal Expansion Board" },
-	{ 0x11, "Other System Board" },
-	{ 0x12, "Processor Board" },
-	{ 0x13, "Power Unit" },
-	{ 0x14, "Power Module" },
-	{ 0x15, "Power Management" },
-	{ 0x16, "Chassis Back Panel Board" },
-	{ 0x17, "System Chassis" },
-	{ 0x18, "Sub-Chassis" },
-	{ 0x19, "Other Chassis Board" },
-	{ 0x1a, "Disk Drive Bay" },
-	{ 0x1b, "Peripheral Bay" },
-	{ 0x1c, "Device Bay" },
-	{ 0x1d, "Fan Device" },
-	{ 0x1e, "Cooling Unit" },
-	{ 0x1f, "Cable/Interconnect" },
-	{ 0x20, "Memory Device" },
-	{ 0x21, "System Management Software" },
-	{ 0x22, "BIOS" },
-	{ 0x23, "Operating System" },
-	{ 0x24, "System Bus" },
-	{ 0x25, "Group" },
-	{ 0x26, "Remote Management Device" },
-	{ 0x27, "External Environment" },
-	{ 0x28, "Battery" },
-	{ 0x00, NULL },
-};
+struct entity_id {
+	unsigned char	id;			/* physical entity id */
+#if WORDS_BIGENDIAN
+	unsigned char	logical     : 1;	/* physical/logical */
+	unsigned char	instance    : 7;	/* instance number */
+#else
+	unsigned char	instance    : 7;	/* instance number */
+	unsigned char	logical     : 1;	/* physical/logical */
+#endif
+} __attribute__ ((packed));
 
-const struct valstr device_type_vals[] __attribute__((unused)) = {
-	{ 0x00, "Reserved" },
-	{ 0x01, "Reserved" },
-	{ 0x02, "DS1624 temperature sensor" },
-	{ 0x03, "DS1621 temperature sensor" },
-	{ 0x04, "LM75 Temperature Sensor" },
-	{ 0x05, "Heceta ASIC" },
-	{ 0x06, "Reserved" },
-	{ 0x07, "Reserved" },
-	{ 0x08, "EEPROM, 24C01" },
-	{ 0x09, "EEPROM, 24C02" },
-	{ 0x0a, "EEPROM, 24C04" },
-	{ 0x0b, "EEPROM, 24C08" },
-	{ 0x0c, "EEPROM, 24C16" },
-	{ 0x0d, "EEPROM, 24C17" },
-	{ 0x0e, "EEPROM, 24C32" },
-	{ 0x0f, "EEPROM, 24C64" },
-	{ 0x1000, "IPMI FRU Inventory" },
-	{ 0x1001, "DIMM Memory ID" },
-	{ 0x1002, "IPMI FRU Inventory" },
-	{ 0x1003, "System Processor Cartridge FRU" },
-	{ 0x11, "Reserved" },
-	{ 0x12, "Reserved" },
-	{ 0x13, "Reserved" },
-	{ 0x14, "PCF 8570 256 byte RAM" },
-	{ 0x15, "PCF 8573 clock/calendar" },
-	{ 0x16, "PCF 8574A I/O Port" },
-	{ 0x17, "PCF 8583 clock/calendar" },
-	{ 0x18, "PCF 8593 clock/calendar" },
-	{ 0x19, "Clock calendar" },
-	{ 0x1a, "PCF 8591 A/D, D/A Converter" },
-	{ 0x1b, "I/O Port" },
-	{ 0x1c, "A/D Converter" },
-	{ 0x1d, "D/A Converter" },
-	{ 0x1e, "A/D, D/A Converter" },
-	{ 0x1f, "LCD Controler/Driver" },
-	{ 0x20, "Core Logic (Chip set) Device" },
-	{ 0x21, "LMC6874 Intelligent Battery controller" },
-	{ 0x22, "Intelligent Batter controller" },
-	{ 0x23, "Combo Management ASIC" },
-	{ 0x24, "Maxim 1617 Temperature Sensor" },
-	{ 0xbf, "Other/Unspecified" },
-	{ 0x00, NULL },
-};
+extern const struct valstr entity_id_vals[];
+extern const struct valstr device_type_vals[];
 
 #endif /* IPMI_ENTITY_H */
-
