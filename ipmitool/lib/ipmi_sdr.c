14042e4021d44b430cad5834a91c31391c60c2f1
Duncan Laurie <duncan@iceblink.org>
2005-03-17 04:39:51 +0000

    sometimes still get valid read when scanning is disabled

--- ipmitool/lib/ipmi_sdr.c
+++ ipmitool/lib/ipmi_sdr.c
@@ -452,6 +452,12 @@ ipmi_sdr_print_sensor_full(struct ipmi_intf * intf,
 			/* Sensor Scanning Disabled
 			 * not an error condition so return 0 */
 			validread = 0;
+			if (rsp->data[0] != 0) {
+				/* we might still get a valid reading */
+				val = sdr_convert_sensor_reading(sensor, rsp->data[0]);
+				if (val != 0.0)
+					validread = 1;
+			}
 		}
 		else if (rsp->data[0] != 0) { 
 			/* convert RAW reading into units */
