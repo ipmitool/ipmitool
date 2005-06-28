86d54446e92784def50c6d18e70bd197e92daf3b
Duncan Laurie <duncan@iceblink.org>
2005-06-28 21:14:51 +0000

    fix for freebsd

--- ipmitool/lib/helper.c
+++ ipmitool/lib/helper.c
@@ -34,17 +34,20 @@
  * facility.
  */
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>  /* For TIOCNOTTY */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <inttypes.h>
 #include <signal.h>
 #include <string.h>
 #include <strings.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <paths.h>  /* For _PATH_TTY */
 
 #include <ipmitool/helper.h>
 #include <ipmitool/log.h>
