1ad5d3d0b2a53bac0be4b3eaca85ed39b7e15142
Duncan Laurie <duncan@iceblink.org>
2006-04-27 18:04:09 +0000

    remove a few straggling nuclear clauses

--- ipmitool/lib/ipmi_fwum.c
+++ ipmitool/lib/ipmi_fwum.c
@@ -15,11 +15,11 @@
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
  *
- * Neither the name of Sun Microsystems, Inc. or the names of                         
+ * Neither the name of Sun Microsystems, Inc. or the names of
  * contributors may be used to endorse or promote products derived
  * from this software without specific prior written permission.
- *														   
- * This software is provided "AS IS," without a warranty of any kind.
+ *
+  * This software is provided "AS IS," without a warranty of any kind.
  * ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES,
  * INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
  * PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED.
@@ -31,10 +31,6 @@
  * PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF
  * LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE,
  * EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- *
- * You acknowledge that this software is not designed or intended for use
- * in the design, construction, operation or maintenance of any nuclear
- * facility.
  */
 
 
@@ -1496,6 +1492,3 @@ tKFWUM_Status KfwumOutputInfo(tKFWUM_BoardInfo boardInfo,
                    firmInfo.versMinor, firmInfo.versSubMinor, firmInfo.sdrRev);
 
 }
-
-
-
