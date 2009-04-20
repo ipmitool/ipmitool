ccb0aba6b9861f6a10a917f62ce47c98d54d3f5d
Francois Isabelle <isabellf@users.sourceforge.net>
2009-04-20 15:08:44 +0000

    Change device id mask (IPM_DEV_DEVICE_ID_REV_MASK) 0x07 for 0x0F.
    As per in IPMI spec V2:
    
    Table 20-2 Get Device Id command byte 3:
    
    [7] 1 = device provides Device SDRs
        0 = device does not provide Device SDRs
    [6:4] reserved. Return as 0.
    [3:0] Device Revision, binary encoded.
    
    The mask need to be done on 4 bits.

--- ipmitool/include/ipmitool/ipmi_mc.h
+++ ipmitool/include/ipmitool/ipmi_mc.h
@@ -1,21 +1,21 @@
 /*
  * Copyright (c) 2003 Sun Microsystems, Inc.  All Rights Reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * Redistribution of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
- * 
+ *
  * Redistribution in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 
+ *
  * Neither the name of Sun Microsystems, Inc. or the names of
  * contributors may be used to endorse or promote products derived
  * from this software without specific prior written permission.
- * 
+ *
  * This software is provided "AS IS," without a warranty of any kind.
  * ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES,
  * INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
@@ -48,9 +48,9 @@
 
 int ipmi_mc_main(struct ipmi_intf *, int, char **);
 
-/* 
+/*
  * Response data from IPM Get Device ID Command (IPMI rev 1.5, section 17.1)
- * The following really apply to any IPM device, not just BMCs... 
+ * The following really apply to any IPM device, not just BMCs...
  */
 struct ipm_devid_rsp {
 	uint8_t device_id;
@@ -65,7 +65,7 @@ struct ipm_devid_rsp {
 } __attribute__ ((packed));
 
 #define IPM_DEV_DEVICE_ID_SDR_MASK     (0x80)	/* 1 = provides SDRs      */
-#define IPM_DEV_DEVICE_ID_REV_MASK     (0x07)	/* BCD-enoded             */
+#define IPM_DEV_DEVICE_ID_REV_MASK     (0x0F)	/* BCD-enoded             */
 
 #define IPM_DEV_FWREV1_AVAIL_MASK      (0x80)	/* 0 = normal operation   */
 #define IPM_DEV_FWREV1_MAJOR_MASK      (0x3f)	/* Major rev, BCD-encoded */
