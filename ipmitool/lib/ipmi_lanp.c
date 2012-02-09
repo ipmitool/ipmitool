403c86e51c1b87f5938c19aaf90ce2f3499579ea
Zdenek Styblik <zdenek.styblik@gmail.com>
2012-02-09 12:40:40 +0000

    Commit fixes wrong evaluation of is_lan_channel() in find_lan_channel() introduced by yesterday's changes of 'lib/ipmi_lanp.c'.

--- ipmitool/lib/ipmi_lanp.c
+++ ipmitool/lib/ipmi_lanp.c
@@ -94,7 +94,7 @@ find_lan_channel(struct ipmi_intf * intf, uint8_t start)
 	uint8_t chan = 0;
 
 	for (chan = start; chan < IPMI_CHANNEL_NUMBER_MAX; chan++) {
-		if (!is_lan_channel(intf, chan)) {
+		if (is_lan_channel(intf, chan)) {
 			return chan;
 		}
 	}
