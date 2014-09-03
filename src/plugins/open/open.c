f49c9eec535b7f80c7b17854e75f86f655d7d922
Parthasarathy Bhuvaragan <parthasarathy.xx.bhuvaragan@ericsson.com>
2014-09-03 10:01:07 +0200

    print message header and data in verbose print
    Change-Id: I613e35e180c815279d2a50f44956a5ed5bf5461b
    Signed-off-by: Parthasarathy Bhuvaragan <parthasarathy.xx.bhuvaragan@ericsson.com>

--- src/plugins/open/open.c
+++ src/plugins/open/open.c
@@ -188,9 +188,14 @@ ipmi_openipmi_send_cmd(struct ipmi_intf * intf, struct ipmi_rq * req)
 		if (intf->open(intf) < 0)
 			return NULL;
 
-	if (verbose > 2)
-		printbuf(req->msg.data, req->msg.data_len,
-			 "OpenIPMI Request Message");
+	if (verbose > 2) {
+		fprintf(stderr, "OpenIPMI Request Message Header:\n");
+		fprintf(stderr, "  netfn     = 0x%x\n",  req->msg.netfn );
+		fprintf(stderr, "  cmd       = 0x%x\n", req->msg.cmd);
+		printbuf(req->msg.data, req->msg.data_len, "OpenIPMI Request Message Data");
+	}
+		
+
 
 	/*
 	 * setup and send message
