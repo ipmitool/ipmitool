99721b52e4c8dd74cc5b96d8313c1229559d26af
Zdenek Styblik <stybla@turnovfree.net>
2015-01-19 06:42:05 +0100

    Fix resource leak in fru_area_print_chassis() - CID#1149047

--- lib/ipmi_fru.c
+++ lib/ipmi_fru.c
@@ -958,9 +958,13 @@ fru_area_print_chassis(struct ipmi_intf * intf, struct fru_info * fru,
 		}
 	}
 
-	if (fru_area != NULL) {
+	if (fru_data != NULL) {
 		free(fru_data);
 		fru_data = NULL;
+	}
+	if (fru_area != NULL) {
+		free(fru_area);
+		fru_area = NULL;
 	}
 }
 
