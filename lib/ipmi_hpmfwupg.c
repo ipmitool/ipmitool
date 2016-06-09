9341982a381ac4b0785e3fefe4b334f1c91f9385
Zdenek Styblik <stybla@turnovfree.net>
2016-06-09 11:48:29 +0200

    ID:335 - Check return value of fseek(), prevent segfault
    Commit adds a check as a workaround for segfault reported in ID:335. However,
    the proper fix is to rewrite parts of lib/ipmi_hpmfwupg.c not to put whole
    firmware image into memory. Also, MD5 checksum part needs to be rewritten,
    because it won't work for large firmware images.

--- lib/ipmi_hpmfwupg.c
+++ lib/ipmi_hpmfwupg.c
@@ -1399,7 +1399,12 @@ HpmfwupgGetBufferFromFile(char *imageFilename,
 		return HPMFWUPG_ERROR;
 	}
 	/* Get the raw data in file */
-	fseek(pImageFile, 0, SEEK_END);
+	ret = fseek(pImageFile, 0, SEEK_END);
+	if (ret != 0) {
+		lprintf(LOG_ERR, "Failed to seek in the image file '%s'",
+				imageFilename);
+		return HPMFWUPG_ERROR;
+	}
 	pFwupgCtx->imageSize  = ftell(pImageFile);
 	pFwupgCtx->pImageData = malloc(sizeof(unsigned char)*pFwupgCtx->imageSize);
 	if (pFwupgCtx->pImageData == NULL) {
