bd0475ce4ad85645f893010423b265ada1514cc5
Alexander Amelkin <alexander@amelkin.msk.ru>
2019-06-11 19:33:35 +0300

    Load IANA PEN registry from a file
    Previously, the OEM names dictionary was compiled in and
    updating it required rebuilding of `ipmitool`, thus taking a
    long time for newly registered OEMs to get supported by the tool.
    
    Building also required a direct internet connection to succeed.
    
    With this commit, the OEM enterprise dictionary is now loaded from
    either ${HOME}/.local/usr/share/misc/enterprise-numbers or from
    /usr/share/misc/enterprise-numbers (in that precedence).
    
    Those files can be downloaded from iana.org at
    http://www.iana.org/assignments/enterprise-numbers
    
    Partially resolves ipmitool/ipmitool#11
    
    Fixes: 9d41136c9b7c7d392f1a3f3adeb6d7fe3bd3135e
    Signed-off-by: Alexander Amelkin <alexander@amelkin.msk.ru>

--- include/ipmitool/ipmi_strings.h
+++ include/ipmitool/ipmi_strings.h
@@ -53,8 +53,10 @@ extern const struct valstr ipmi_chassis_power_control_vals[];
 extern const struct valstr ipmi_auth_algorithms[];
 extern const struct valstr ipmi_integrity_algorithms[];
 extern const struct valstr ipmi_encryption_algorithms[];
-extern const struct valstr ipmi_oem_info[];
 extern const struct valstr ipmi_user_enable_status_vals[];
+extern const struct valstr *ipmi_oem_info;
+int ipmi_oem_info_init();
+void ipmi_oem_info_free();
 
 extern const struct valstr picmg_frucontrol_vals[];
 extern const struct valstr picmg_clk_family_vals[];
